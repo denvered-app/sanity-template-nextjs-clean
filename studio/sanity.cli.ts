3b0f5a34a77eb92569fc08a46976a29f1f9f853e
Jake Williams <info@denvered.com>
2024-12-18 10:57:40 -0600

    changes

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -7,8 +7,8 @@
 
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
-const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'tjtx5fyj'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export default defineCliConfig({
   api: {
